0defc6fbcaaa02ee85604e9296a72b1a5bc6bc9b
tsulemani <sulemani007@gmail.com>
2022-02-01 22:14:24 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,8 @@
 const text = document.getElementById("text");
 const prog = "I know you are ...  ";
+writeText("aaaaaaaaaaaaaaaaaaaaaaaa")
 let idx = 1;
-sleep(5000);
+sleep1(5000);
 setInterval(writeText, 150);
 function writeText() {
     text.innerText = prog.slice(0, idx)
@@ -11,7 +12,7 @@ function writeText() {
 	if(idx > prog.length){
       idx = 1;
     }
-	function sleep(miliseconds) {
+	function sleep1(miliseconds) {
    var currentTime = new Date().getTime();
 
    while (currentTime + miliseconds >= new Date().getTime()) {
