ecf0c0f327ece5f78ad6cdf1ba79273aa9f27ba8
tsulemani <sulemani007@gmail.com>
2022-02-01 22:28:42 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 const text = document.getElementById("text");
-const prog = "Happy Birthday Riya... /n I know you are ...  ";
+const prog = "Happy Birthday Riya... \n I know you are ...  ";
 let idx = 1;
 setInterval(writeText, 130);
 function writeText() {
