737e03243288e5423efc22477f6f6960afcec31d
tsulemani <sulemani007@gmail.com>
2022-02-01 22:45:29 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 const text = document.getElementById("text");
-const prog = "Happy Birthday Riya... \n I know you are not much into birthday celebration. You will be like why did you do this, why did did you put this much of effort. \n To be honest I didn't do anything. I had this idea for so long that I couldn't let it go. \n Anyways, it's your birthday, I hope that you have a better year ahead, I better not write those not so useful long paragraphs...";
+const prog = "Happy Birthday Riya... \n I know you are not much into birthday celebration. \n You will be like why did you do this, why did you put this much of effort. \n To be honest I didn't do anything. \n I had this idea for so long that I couldn't let it go. \n Anyways, it's your birthday, I hope that you have a better year ahead.\n I better not write those not so useful long paragraphs...";
 let idx = 1;
 setInterval(writeText, 130);
 function writeText() {
